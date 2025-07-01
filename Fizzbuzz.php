351644e68ecd46c008ef91870af4bf0eeb5567d2
Bastien Nicolau <b.nicolau@terravitagroup.com>
2025-07-01 15:52:12 +0200

    feat: add FizzBuzz implementation

---
+++ Fizzbuzz.php
@@ -0,0 +1,18 @@
+<?php
+
+function fizzbuzz(int $n): void
+{
+    for ($i = 1; $i <= $n; $i++) {
+        if ($i % 15 === 0) {
+            echo "FizzBuzz" . PHP_EOL;
+        } elseif ($i % 3 === 0) {
+            echo "Fizz" . PHP_EOL;
+        } elseif ($i % 5 === 0) {
+            echo "Buzz" . PHP_EOL;
+        } else {
+            echo $i . PHP_EOL;
+        }
+    }
+}
+
+fizzbuzz(100);
